3655a7348f800342bcef90a50c4254b121eb2010
TouchySarun <25566sarun@gmail.com>
2020-04-18 02:31:14 +0700

    edit char from capital to small

--- SPR_Module.hs
+++ SPR_Module.hs
@@ -2,9 +2,9 @@ import System.Random
 
 random_spr = do
    randomIndex <- randomRIO (0,2)
-   print $ ['S','P','R'] !! randomIndex
+   print $ ['s','p','r'] !! randomIndex
 
 spr x y 
     | x == y = "draw"
-    | (x == 'S' && y == 'P') || (x == 'P' && y == 'R') || (x == 'R' && y == 'S') = "win"
+    | (x == 's' && y == 'p') || (x == 'p' && y == 'r') || (x == 'r' && y == 's') = "win"
     | otherwise = "lose"
