d1d0f79b7f7584dce69896dea61f03ffbd5a9a0a
noratap09 <35356878+noratap09@users.noreply.github.com>
2020-04-18 17:39:17 +0700

    แก้ random_spr
    เดิม random_spr :: IO () เป็น random_spr :: IO Char
    เวลาใช้ x <- random_spr ได้ x :: Char

--- SPR_Module.hs
+++ SPR_Module.hs
@@ -2,7 +2,7 @@ import System.Random
 
 random_spr = do
    randomIndex <- randomRIO (0,2)
-   print $ ['s','p','r'] !! randomIndex
+   return(['s','p','r'] !! randomIndex)
 
 spr x y 
     | x == y = "draw"
