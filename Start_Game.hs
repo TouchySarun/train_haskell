eafaa40d5b27af019c2c0eec7236d3f15a2bb956
noratap09 <35356878+noratap09@users.noreply.github.com>
2020-04-23 18:01:14 +0700

    แก้นิดหน่อย

--- Start_Game.hs
+++ Start_Game.hs
@@ -11,22 +11,22 @@ gen_table _ _ table = table
 
 start_game = do
     table_status <- load
-    if gameover table_status
+    if gameover table_status 
     then 
         do
         getlong <- get_long
         gethigh <- get_high
         let table_status = gen_table get_long get_high table_status
         print "Gen Game"
-    else 
-        do
-        let loop = do
-            getchoice <- get_choice
-            randomSPR <- random_spr
-            let ck = spr getchoice randomSPR
-            print randomSPR
-            case ck of
-                "lose" -> loop
-                "draw" -> loop
-                "win" -> print "End Game"
-        loop  -- start the first iteration
+    else
+        return()
+    let loop = do
+        getchoice <- get_choice
+        randomSPR <- random_spr
+        let ck = spr getchoice randomSPR
+        print randomSPR
+        case ck of
+            "lose" -> loop
+            "draw" -> loop
+            "win" -> print "End Game"
+    loop  -- start the first iteration
